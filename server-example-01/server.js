87a0da9c54b6c1a70102b56cadd1eda8a8fe8376
Nicolás Navarro <nicohnavarro@gmail.com>
2024-08-17 00:26:56 +0200

    🔌  Test Socket

---
+++ server-example-01/server.js
@@ -0,0 +1,9 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const socket = new Server(httpServer, {});
+
+socket.on("connection", (socket) => console.log(socket));
+
+httpServer.listen(3000, () => console.log("Server is connected! 🔌"));
